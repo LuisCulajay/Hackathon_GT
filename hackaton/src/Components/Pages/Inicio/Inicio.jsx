cf6f94258eb48fd34d9aad3aea8b721bf737397b
Adriana <adriana.d@hotmail.com>
2023-02-16 21:54:38 -0600

    se agregó mision y vision

--- hackaton/src/Components/Pages/Inicio/Inicio.jsx
+++ hackaton/src/Components/Pages/Inicio/Inicio.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Grid, Card, Text } from "@nextui-org/react";
+import { Grid, Card } from "@nextui-org/react";
+import { Collapse, Text, Image } from "@nextui-org/react";
 
 
 
 import Barra from "../../Barra/Barra";
+import imagen1 from './img1.jpeg'
 
 
 
@@ -28,7 +30,6 @@ function Inicio ()  {
       <br></br>
 
 
-
       <Text h1 size={60} css={{ textGradient: "45deg, $cyan500 -20%, $blue600 50%", }} weight="bold">
         ¿Quiénes somos? 
       </Text>
@@ -38,23 +39,30 @@ function Inicio ()  {
         
 
         <Grid xs={6} justify='center'>
-          <Text h1 weight="bold" >
-            Misión
-          </Text>
         </Grid>
         
         <Grid xs={6} justify='center'>
-          <Text h1 weight="bold">
-            Visión
-          </Text>
         </Grid>
         
-        <Grid xs={6}>
-          <MockItem text="aqui la misión" />
+        <Grid xs={6}>    
+
+          <Collapse.Group accordion={false}>
+            <br></br><br></br><br></br><br></br>
+            <Collapse title="Misión">
+              <Text>
+              Somos reconocidos por nuestra excelecia en el servicio como el Grupo Financiero No. 1 en crecimiento y rentabilidad, a través del desarrollo de colaboradores, comprometidos en generar el máximo bienestar de sus clientes.
+              </Text>
+            </Collapse>
+            <Collapse title="Visión">
+              <Text>
+                Nuestra pasión por el servicio, RESUELVE YA!
+              </Text>
+            </Collapse>
+          </Collapse.Group>
         </Grid>
 
         <Grid xs={6}>
-          <MockItem text="aqui la visión" />
+          <Image src={imagen1}/>
         </Grid>
         
 
